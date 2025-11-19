563c75580efbded8b941ae260d2d586e872686bc
Kai Akkermann <kai.akkermann@kighlander.de>
2025-11-19 21:30:15 +0100

    added favicon.ico

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Kigh-Anzeigen')</title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
     @vite([
         'resources/css/header.css',
